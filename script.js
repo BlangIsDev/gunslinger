7bc225d88a55dc4c9bea3d99b3df7a6638f14158
Paul Jeung <jeungpjw104@gmail.com>
2025-11-06 13:10:52 -0500

    left and right

--- script.js
+++ script.js
@@ -1,36 +1,62 @@
 //player
 var player = document.querySelector(".player");
 
-ground = window.innerHeight;
-gravity = 5;
+var ground = window.innerHeight;
+var wall = window.innerWidth;
 
+var gravity = 5;
+var speed = 10;
 var grounded = false;
 
+var keys = {
+    right: false,
+    left: false
+};
 
-    function gameLoop() {
+//key detection
+window.addEventListener("keydown", (e) => {
+    if (e.code === "ArrowRight") keys.right = true;
+    if (e.code === "ArrowLeft") keys.left = true;
+});
 
-        //x and y positions
-        var playhit = player.getBoundingClientRect();
+window.addEventListener("keyup", (e) => {
+    if (e.code === "ArrowRight") keys.right = false;
+    if (e.code === "ArrowLeft") keys.left = false;
+});
 
-        var ypos = playhit.top;
-        var xpos = playhit.left;
-        var ybottom = playhit.bottom;
-        var xright = playhit.right;
 
-        //gravity
-        if(ybottom < ground){
-            ypos += gravity;
-            grounded = false;
-        }
-        else{
-            
-            grounded = true;
-        }
+function gameLoop() {
 
-        //update player position
-        player.style.top = ypos + "px";
-        player.style.left = xpos + "px";
-        
-        requestAnimationFrame(gameLoop);
+    //x and y positions
+    var playhit = player.getBoundingClientRect();
+
+    var ypos = playhit.top;
+    var xpos = playhit.left;
+    var ybottom = playhit.bottom;
+    var xright = playhit.right;
+
+    //gravity
+    if (ybottom < ground) {
+        ypos += gravity;
+        grounded = false;
+    }
+    else {
+
+        grounded = true;
+    }
+
+    //movement
+    if (keys.right == true && xright < wall) {
+        xpos += speed;
+    }
+    if (keys.left == true && xpos > 0) {
+        xpos -= speed;
+    }
+
+    //update player position
+    player.style.top = ypos + "px";
+    player.style.left = xpos + "px";
+
+    requestAnimationFrame(gameLoop);
 }
-    gameLoop();
+gameLoop();
