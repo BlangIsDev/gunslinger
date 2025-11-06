a6a541d2f841641f0486a9f64498e2bce9df2253
Paul Jeung <jeungpjw104@gmail.com>
2025-11-06 11:51:35 -0500

    added gravity (hopefully works)

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
-/player
-var player=document.querySelector(".player");
+//player
+var player = document.querySelector(".player");
 
-/x and y positions
-var ypos=player.style.top;
-var xpos=player.style.left; 
+//x and y positions
+var playhit = player.getBoundingClientRect();
+
+var ypos = playhit.top;
+var xpos = playhit.left;
+var ybottom = playhit.bottom;
+var xright = playhit.right;
+
+var grounded;
+
+
+    function gameLoop() {
+
+        //gravity
+        if(ybottom >= 0){
+            ypos + 1;
+        }
+        requestAnimationFrame(gameLoop);
+}
+    gameLoop();
