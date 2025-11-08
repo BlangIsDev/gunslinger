51593e9ee18ce627a6d71b539106b2eca8ae2aac
Paul Jeung <jeungpjw104@gmail.com>
2025-11-07 20:31:05 -0500

    added dash

--- script.js
+++ script.js
@@ -26,7 +26,8 @@ var grounded = false;
 var keys = {
     right: false,
     left: false,
-    space: false
+    space: false,
+    dashdown: false
 };
 
 //control keys
@@ -34,6 +35,7 @@ const controls = {
   right: ["ArrowRight", "KeyD"],
   left: ["ArrowLeft", "KeyA"],
   jump: ["Space", "KeyW", "ArrowUp"]
+  dashdown: ["KeyX"]
 };
 
 
@@ -41,11 +43,8 @@ const controls = {
 window.addEventListener("keydown", (e) => {
     if (controls.right.includes(e.code)) keys.right = true;
     if (controls.left.includes(e.code)) keys.left = true;
-    if (controls.jump.includes(e.code) && jumpsleft > 0) {
-        yvelocity = jumpvelocity;
-        jumpsleft--;
-        playerimg.src = "greenbeanboom.png"
-    }
+    if (controls.jump.includes(e.code) && jumpsleft > 0) keys.space = true;
+    if (controls.dashdown.includes(e.code)) keys.dash = true
 });
 
 window.addEventListener("keyup", (e) => {
@@ -59,16 +58,21 @@ window.addEventListener("keyup", (e) => {
 
 function gameLoop() {
 
+        //update hitbox
+
     playhit = player.getBoundingClientRect();
 
     //gravity
     yvelocity += gravity;
     ypos += yvelocity;
 
+    //ground detection
+
     if (ypos + playerheight >= ground) {
         ypos = ground - playerheight;
         yvelocity = 0;
         jumpsleft = 2;
+        speed = 5;
         grounded = true;
     }
     else {
@@ -82,10 +86,20 @@ function gameLoop() {
     if (keys.left == true && xpos > 0) {
         xpos -= speed;
     }
+    if (keys.space == true) {
+        yvelocity = jumpvelocity;
+        jumpsleft--;
+        playerimg.src = "greenbeanboom.png"
+    }
+    if (keys.dashdown == true && grounded == false) {
+        gravity = 5;
+        speed = 0;
 
     //update player position
     player.style.top = ypos + "px";
     player.style.left = xpos + "px";
+
+    //loop the game
 
     requestAnimationFrame(gameLoop);
 }
